4c87e1212fb9cfbed0b9062eebd3b48206db0757
koko37 <engel0088@gmail.com>
2024-12-06 11:01:27 +0800

    update link-generation

--- link.js
+++ link.js
@@ -1,7 +1,10 @@
 const { nanoid } = require("nanoid");
 
-function mergeToBase64(password, passcode) {
-  const merged = Buffer.from(password + "\u0002" + passcode, "utf-8");
+function mergeToBase64(password, passcode, network) {
+  const merged = Buffer.from(
+    password + "\u0002" + passcode + "\u0002" + network,
+    "utf-8"
+  );
   const base64Encoded = merged.toString("base64");
   const padding = nanoid(12);
   return padding.slice(0, 6) + base64Encoded + padding.slice(6);
@@ -10,16 +13,16 @@ function mergeToBase64(password, passcode) {
 function splitFromBase64(token) {
   const payload = token.slice(6, token.length - 6);
   const base64Decoded = Buffer.from(payload, "base64").toString("utf-8");
-  const [password, passcode] = base64Decoded.split("\u0002");
-  return [password, passcode];
+  const [password, passcode, network] = base64Decoded.split("\u0002");
+  return [password, passcode, network];
 }
 
 async function main() {
-  const token = mergeToBase64("DemoAccount5&", "112324");
-  console.log('token: ', token)
+  const token = mergeToBase64("DemoAccount5&", "112324", "polygon");
+  console.log("token: ", token);
 
-  const [password, passcode] = splitFromBase64(token);
-  console.log(password, passcode);
+  const [password, passcode, network] = splitFromBase64(token);
+  console.log(password, passcode, network);
 }
 
 main().then(() => {
